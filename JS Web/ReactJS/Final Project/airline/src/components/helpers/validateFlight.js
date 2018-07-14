6dda36a9c783fbe2ac83261776ec5e81b6722d36
kosyovachkov <kosyo.vachkov@gmail.com>
2018-07-14 14:21:51 +0300

    Refactored validate method.

--- JS Web/ReactJS/Final Project/airline/src/components/helpers/validateFlight.js
+++ JS Web/ReactJS/Final Project/airline/src/components/helpers/validateFlight.js
@@ -1,52 +1,41 @@
 import observer from '../../scripts/observer';
 
-let validate1 = data=>{
-  
-    if (data.destination.length < 3) {
-      observer.trigger(observer.events.notification, {
-        type: 'error',
-        message: 'Destination must be at least 3 characters'
-      });
-      return false;
-    } else if (data.origin.length < 3) {
-      observer.trigger(observer.events.notification, {
-        type: 'error',
-        message: 'Origin must be at least 3 characters'
-      });
-      return false;
-    } else if (data.departureDate === '') {
-      observer.trigger(observer.events.notification, {
-        type: 'error',
-        message: 'You must set appropriate date'
-      });
-      return false;
-    } else if (data.departureTime === '') {
-      observer.trigger(observer.events.notification, {
-        type: 'error',
-        message: 'You must set appropriate time'
-      });
-      return false;
-    } else if (data.seats < 10) {
-      observer.trigger(observer.events.notification, {
-        type: 'error',
-        message: 'Number of seat must be greater than 10'
-      });
-      return false;
-    } else if (data.cost <= 0) {
-      observer.trigger(observer.events.notification, {
-        type: 'error',
-        message: 'Cost per seat must be greater than $0'
-      });
-      return false;
-    } else if (data.public !== 'yes' && data.public !== 'no') {
-      observer.trigger(observer.events.notification, {
-        type: 'error',
-        message: 'Write "yes" for public, or "no" for nonpublic'
-      });
-      return false;
-    }
-    return true;
-  
+let messages = {
+  destination: 'Destination must be at least 3 characters',
+  origin: 'Origin must be at least 3 characters',
+  departureDate: 'You must set appropriate date',
+  departureTime: 'You must set appropriate time',
+  seats: 'Number of seat must be greater than 10',
+  cost: 'Cost per seat must be greater than $0',
+  public: 'Write "yes" for public, or "no" for nonpublic'
 };
 
-export default validate1;
+let obs = mes => observer.trigger(observer.events.notification, { type: 'error', message: mes });
+
+let validate = data => {
+  if (data.destination.length < 3) {
+    obs(messages['destination']);
+    return false;
+  } else if (data.origin.length < 3) {
+    obs(messages['origin']);
+    return false;
+  } else if (data.departureDate === '') {
+    obs(messages['departureDate']);
+    return false;
+  } else if (data.departureTime === '') {
+    obs(messages['departureTime']);
+    return false;
+  } else if (data.seats < 10) {
+    obs(messages['seats']);
+    return false;
+  } else if (data.cost <= 0) {
+    obs(messages['cost']);
+    return false;
+  } else if (data.public !== 'yes' && data.public !== 'no') {
+    obs(messages['public']);
+    return false;
+  }
+  return true;
+};
+
+export default validate;
