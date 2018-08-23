e51957804151503affd0a39f00f6c2321418d902
kosyovachkov <kosyo.vachkov@gmail.com>
2018-08-23 21:42:30 +0300

    remove kinvey auth...

--- JS Web/Angular 2/Airline/src/app/authentication/kinvey.auth.ts
+++ JS Web/Angular 2/Airline/src/app/authentication/kinvey.auth.ts
@@ -1,5 +1,5 @@
 export const kinveyAuth = {
-    appKey: 'kid_SJgvk38S7', // APP KEY HERE;
-    appSecret: '5eb84974e9b84cce9447be19839050c2',// APP SECRET HERE;
-    appMasterSecret: 'faaf2075c63d438aab8099aaa9a0c60c'
+    appKey: '', // APP KEY HERE;
+    appSecret: '',// APP SECRET HERE;
+    appMasterSecret: ''
 }
